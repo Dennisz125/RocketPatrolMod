1aba14e52c323ec269da56e4aebacd33ab86a714
Dennisz125 <denzabluda@email.com>
2021-04-10 14:20:38 -0700

    Add Spaceship & it's features

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,12 +1,10 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
-  
-      
-      scene.add.existing(this);     // add object to existing scene
-      this.isFiring = false;        // track Rocket's firing status
-      this.moveSpeed = 2;           // pixel per frame
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);     // add object to existing scene
+        this.isFiring = false;        // track Rocket's firing status
+        this.moveSpeed = 2;           // pixel per frame
     }
 
     update() {
